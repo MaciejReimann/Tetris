d6ecaeea821939039f972e1ecd77e59612eb534b
MaciejReimann <maciej.reimann@gmail.com>
2018-05-05 20:20:36 +0200

    Code totally refactored; TODO: colors, rotation on keystroke, and all the other tetris

--- script.js
+++ script.js
@@ -30,147 +30,128 @@ function createCanvas(width, height, parent, className) {
 view.canvas = createCanvas(config.width, config.height, view.main, 'canvas');
 const ctx = view.canvas.getContext('2d');
 
-function Module([posX, posY]) {
-  this.mod = config.mod;
-  this.posX = posX * this.mod;
-  this.posY = posY * this.mod;
-  this.color;  
+function clearCanvas() {
+  ctx.clearRect(0, 0, config.width, config.height);
 }
 
-Module.prototype.clear = function() {
-  ctx.clearRect(this.posX, this.posY, this.mod, this.mod);
-};
-Module.prototype.place = function() {  
-  ctx.fillRect(this.posX, this.posY, this.mod, this.mod);
-};
-Module.prototype.setColor = function(color) {  
+
+function Square(x,y) {
+  this.mod = config.mod;
+  this.anchorPoint = {
+    x: x,
+    y: y
+  }
+}
+Square.prototype.draw = function(color) {
+  this.color = color;
+  ctx.fillRect(this.anchorPoint.x, this.anchorPoint.y, this.mod, this.mod);
   ctx.fillStyle = color;
 };
-Module.prototype.moveRight = function() {
-  this.clear();  
-  this.posX = this.posX + this.mod;
-  this.place();
-};
-Module.prototype.moveLeft = function() {
-  this.clear();  
-  this.posX = this.posX - this.mod;
-  this.place();
-};
-Module.prototype.moveDown = function() {
-  this.clear();  
-  this.posY = this.posY + this.mod;
-  this.place();
-};
-Module.prototype.moveUp = function() {
-  this.clear();  
-  this.posY = this.posY - this.mod;
-  this.place();
-};
 
-function L_Shape(x,y) {
+function L_ShapeLeft(x,y) {
+  Tetris.call(this, x, y);
+  this.getAnchorPoints = function () {
+    let anchorPoints = [
+      {
+        x: this.pivot.x - this.mod * 1.5,
+        y: this.pivot.y - this.mod
+      },
+      {
+        x: this.pivot.x - this.mod * 1.5,
+        y: this.pivot.y
+      },
+      {
+        x: this.pivot.x - this.mod * 0.5,
+        y: this.pivot.y
+      },
+      {
+        x: this.pivot.x + this.mod * 0.5,
+        y: this.pivot.y
+      },
+    ];
+  return anchorPoints;
+  }
+}
+L_ShapeLeft.prototype = Object.create(Tetris.prototype);
+
+function L_ShapeRight(x,y) {
+  Tetris.call(this, x, y);
+  this.getAnchorPoints = function () {
+    let anchorPoints = [
+      {
+        x: this.pivot.x - this.mod * 1.5,
+        y: this.pivot.y
+      },
+      {
+        x: this.pivot.x - this.mod * 0.5,
+        y: this.pivot.y
+      },
+      {
+        x: this.pivot.x + this.mod * 0.5,
+        y: this.pivot.y
+      },
+      {
+        x: this.pivot.x + this.mod * 0.5,
+        y: this.pivot.y - this.mod
+      },
+    ];
+    return anchorPoints;
+  }
+}
+L_ShapeRight.prototype = Object.create(Tetris.prototype);
+
+function Tetris(x,y) {
   this.mod = config.mod;
   this.pivot = {
     x: x, 
     y: y
   };
-  this._1 = {
-    x: this.pivot.x - this.mod * 1.5,
-    y: this.pivot.y - this.mod
-  };
-  this._2 = {
-    x: this.pivot.x - this.mod * 1.5,
-    y: this.pivot.y
-  };
-  this._3 = {
-    x: this.pivot.x - this.mod * 0.5,
-    y: this.pivot.y
-  };
-  this._4 = {
-    x: this.pivot.x + this.mod * 0.5,
-    y: this.pivot.y
-  };
+}
+Tetris.prototype.draw = function() {
+  clearCanvas();
+  this.squares = [];
+  this.getAnchorPoints().forEach((point) => this.squares.push(new Square(point.x, point.y)));
+  this.squares.forEach((square) => square.draw() )
+}
+Tetris.prototype.moveUp = function() {
+  this.pivot.y = this.pivot.y - this.mod;
+  this.draw();
+}
+Tetris.prototype.moveRight = function() {
+  this.pivot.x = this.pivot.x + this.mod;
+  this.draw();
+}
+Tetris.prototype.moveDown = function() {
+  this.pivot.y = this.pivot.y + this.mod;
+  this.draw();
+}
+Tetris.prototype.moveLeft = function() {
+  this.pivot.x = this.pivot.x - this.mod;
+  this.draw();
 }
 
-const lShape = new L_Shape(config.entryX, config.entryY)
 
 
-function Square_Shape(){
-  this._1 = [this.entryX-1, this.entryY];
-  this._2 = [this.entryX, this.entryY];
-  this._3 = [this.entryX-1, this.entryY+1];
-  this._4 = [this.entryX, this.entryY+1];
-  // this.pivotPoint = {x: , y:};
-}
-function I_Shape(){
-  this._1 = [this.entryX-2, this.entryY];
-  this._2 = [this.entryX-1, this.entryY];
-  this._3 = [this.entryX, this.entryY];
-  this._4 = [this.entryX+1, this.entryY];
-  // this.pivotPoint = {x: , y:};
-}
-I_Shape.prototype = new Shape();
-Square_Shape.prototype = new Shape();
+// const lShape = new L_ShapeRight(config.entryX, config.entryY)
+// lShape.draw();
 
-function Shape() {
-  this.entryX = config.entryX;
-  this.entryY = config.entryY;
-}
-Shape.prototype.getPivot = function() {
-  return [this.shape[2].posX, this.shape[2].posY + config.mod];
-}
-Shape.prototype.create = function() {  
-  this.shape = [
-    new Module(this._4), new Module(this._3), new Module(this._2), new Module(this._1)
-  ];
-}
-Shape.prototype.paint = function(color) {
- this.color = color;
- this.shape.forEach( (square) => {square.setColor(this.color)} );
-}
-Shape.prototype.place = function() {
- this.shape.forEach( (square) => {square.place()});
-}
-Shape.prototype.rotate = function(angle) {
-  this.paint(this.color);
-  view.angle = view.angle + angle;
-  updateCoordinates();
-  let centreX = this.pivotPoint.x;
-  let centreY = this.pivotPoint.y;
-  this.shape.forEach( (square) => {square.clear()} )
-  ctx.translate(centreX, centreY);
-  ctx.rotate(angle * Math.PI / 180);
-  this.shape.forEach( (square) => {
-    square.posX = square.posX-centreX;
-    square.posY = square.posY-centreY;
-    square.place();      
-  });
-}
-Shape.prototype.move = function() {
-  this.copy = this.shape.slice();  
-  this.left = function() {this.copy.reverse().forEach( (square) => {square.moveLeft()} )}.bind(this);
-  this.right = function() {this.shape.forEach( (square) => {square.moveRight()} )}.bind(this);
-  this.down = function() {this.shape.forEach( (square) => {square.moveDown()} )}.bind(this);
-  this.up = function() {this.shape.forEach( (square) => {square.moveUp()} )}.bind(this);
-  checkIfHitTheBottom();
-  return {
-    left: this.left,
-    right: this.right,
-    down: this.down,
-    up: this.up
-  }
-}
 
-Shape.prototype.drawLineOnCanvas = function() {
-  this.mod = config.mod;
-  this.startMiddleAxisX = this.entryX*this.mod;
-  this.startMiddleAxisY = this.entryY*this.mod;
-  this.endMiddleAxisX = this.entryX*this.mod;
-  this.endMiddleAxisY = this.entryY*this.mod + config.height;
-  ctx.beginPath();
-  ctx.moveTo(this.startMiddleAxisX, this.startMiddleAxisY);
-  ctx.lineTo(this.endMiddleAxisX, this.endMiddleAxisY);
-  ctx.stroke();
-}
+// Shape.prototype.rotate = function(angle) {
+//   this.paint(this.color);
+//   view.angle = view.angle + angle;
+//   updateCoordinates();
+//   let centreX = this.pivotPoint.x;
+//   let centreY = this.pivotPoint.y;
+//   this.shape.forEach( (square) => {square.clear()} )
+//   ctx.translate(centreX, centreY);
+//   ctx.rotate(angle * Math.PI / 180);
+//   this.shape.forEach( (square) => {
+//     square.posX = square.posX-centreX;
+//     square.posY = square.posY-centreY;
+//     square.place();      
+//   });
+// }
+
 
 function checkIfHitTheBottom() {
   checkIfFullLine()
@@ -179,45 +160,51 @@ function checkIfFullLine() {
   return true;
 }
 
-
-const activeShape = new I_Shape();
-
-activeShape.create();
-activeShape.paint('pink');
-activeShape.place();
-activeShape.drawLineOnCanvas();
-view.activeItem = activeShape;
+const activeShape = {};
 
 
-const up =  activeShape.move().up;
-const right = activeShape.move().right;
-const down = activeShape.move().down;
-const left = activeShape.move().left;
-let accordingToShape = [];
-function updateCoordinates(){
+
+
+activeShape.set = function(tetris) {
+  this.instance = tetris;
+  this.instance.draw()
+  view.activeShape = this.instance;
+};
+activeShape.rotation = []; // first callback in the array moving tetris up, i.e. north first;
+activeShape.move = function() {
+  let tetris = this.instance;
+  let rotation = this.rotation;
+  const up = function() {tetris.moveUp()};
+  const right = function() {tetris.moveRight()};
+  const down = function() {tetris.moveDown()};
+  const left = function() {tetris.moveLeft()};
   let angle = view.angle%360;
   console.log(angle)
   if(angle===90 || angle === -270) {
-    accordingToShape = [left, up, right, down];
+    rotation = [left, up, right, down];
   } else if(angle===180 || angle===-180) {
-    accordingToShape = [down, left, up, right];
+    rotation = [down, left, up, right];
   } else if (angle===270 || angle===-90) {
-    accordingToShape = [right, down, left, up];
+    rotation = [right, down, left, up];
   } else {
-    accordingToShape = [up, right, down, left]
+    rotation = [up, right, down, left]
+  }
+  return {
+    right: function() {rotation[1]()},
+    down: function() {rotation[2]()},
+    left: function() {rotation[3]()}
   }
 }
-function moveRight() {accordingToShape[1]()};
-function moveDown() {accordingToShape[2]()};
-function moveLeft() {accordingToShape[3]()}
+
+activeShape.set(new L_ShapeRight(config.entryX, config.entryY));
 
 
 const keydownHandler = function() {
   const activeItem = view.activeItem;
   const listenedKeys = {
-    ArrowDown: (function() { moveDown() }),
-    ArrowRight: (function() { moveRight() }),
-    ArrowLeft: (function() {  moveLeft() }),
+    ArrowDown: activeShape.move().down,
+    ArrowRight: activeShape.move().right,
+    ArrowLeft: activeShape.move().left,
     z: (function() { activeItem.rotate(-90) }),
     a: (function() { activeItem.rotate(90) }),
   }
