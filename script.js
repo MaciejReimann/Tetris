b0b016f36d122872bc9614feee935eb428bbe4b1
MaciejReimann <maciej.reimann@gmail.com>
2018-05-27 01:25:05 +0200

    missesModuleOn funciton added - it's used when placing new tetris to correct the starting point

--- script.js
+++ script.js
@@ -137,8 +137,6 @@ const timer = (function () {
   }
 })();
 
-// ----- VERTEX TRANSFORMATION FUNCTIONS ------
-
 const size = view.canvasConfig.modularUnit;
 const centerOfCanvas = {x:200, y: 225}
 const sqr = new Square(size, centerOfCanvas, 45)
@@ -147,115 +145,12 @@ poly.drawOutline(view.largeCanvas.ctx);
 sqr.drawOutline(view.largeCanvas.ctx);
 
 
-
-function getRandomItem(array) {
-  return array[ Math.floor(Math.random() * array.length) ];
-};
-
-function translateToGlobal(localZero, localVertices, mod) { // localZero in global units, localVertices in local units, mod - the size of the local unit
-  if(!mod) { mod = 1 }; // mod will not affect the return values;
-  if(localVertices instanceof Array) {
-    return localVertices.map( (localVertex) => translateToGlobal(localZero, localVertex, mod) );
-  } else {
-    let localVertex = localVertices;
-    return { // vertex in global units
-      x: localZero.x + localVertex.x * mod,
-      y: localZero.y + localVertex.y * mod 
-    };
-  };
-};
-
-function translateToLocal(localZero, globalVertices) {
-  if(globalVertices instanceof Array) {
-    return globalVertices.map( (globalVertex) => translateToLocal(localZero, globalVertex) );
-  } else {
-    let globalVertex = globalVertices;
-    return {
-      x: globalVertex.x - localZero.x,
-      y: globalVertex.y - localZero.y
-    };
-  };
-};
-
-function translateToCartesian(vertices) {
-if(vertices instanceof Array) {
-    return vertices.map( (vertex) => translateToCartesian(vertex) );
-  } else { // TODO: Add argument validation - check if vertices has r and angle property and their value is number, else throw an Error;  
-    let vertex = vertices;
-    let n = 0; // optional angle parameter    
-    return {
-      x: vertex.r * Math.cos( (vertex.angle - n) * (Math.PI / 180) ),
-      y: vertex.r * Math.sin( (vertex.angle - n) * (Math.PI / 180) ),
-    };
-  };
-};
-
-function translateToPolar(vertices) {
-  if(vertices instanceof Array) {
-    return vertices.map( (vertex) => translateToPolar(vertex) );
-  } else { // TODO: Add argument validation - check if vertices has x and y property and their value is number, else throw an Error;  
-    let vertex = vertices;
-    return {
-      r: Math.sqrt(vertex.x * vertex.x + vertex.y * vertex.y),
-      angle: Math.atan2(vertex.y, vertex.x) * (180 / Math.PI),
-    };
-  };
-};
-
-function rotatePolar(polarVertices, angle) {
-  if(polarVertices instanceof Array) {
-    return polarVertices.map( (polarVertex) => rotatePolar(polarVertex, angle) );
-  } else {
-    let polarVertex = polarVertices;
-    return {
-      r: polarVertex.r,
-      angle: polarVertex.angle + angle
-    };
-  };
-};
-
-function rotateCartesian(localVertices, angle) { // if rotate localVertices, pivot is in 0,0; if rotateGlobal pivot needs to be set; pivot = localZero
-  if(localVertices instanceof Array) {
-    return localVertices.map( (localVertex) => rotateCartesian(localVertex, angle));
-  } else {
-    console.log(angle)
-    let localVertex = localVertices;
-    let cartesian = translateToCartesian( 
-      rotatePolar( 
-        translateToPolar(localVertex), 
-      angle) 
-    );
-    return cartesian  
-  };
-};
-
-function mirrorByY_Axis(vertices) {
-  if(vertices instanceof Array) {
-    return vertices.map( (vertex) => mirrorByY_Axis(vertex) );
-  } else {
-    let vertex = vertices;
-    return {
-      x: vertex.x * -1, 
-      y: vertex.y
-    };
-  };
-};
-
-
-function isNotGreaterThen(numbers, end) {
-  if(numbers instanceof Array) {
-   return numbers.every( (number) => isNotGreaterThen(number, end) );
-  } else {
-    let number = numbers;
-    return number <= end;
-  };
-};
-
 // --------------------------------------------------------
 // --------------------------------------------------------
 // ----------------- TETRIS FACTORY  ----------------------
 // --------------------------------------------------------
 // --------------------------------------------------------
+
 
 const tetrisFactory = (function() {
   const modularUnit = view.canvasConfig.modularUnit;
@@ -334,20 +229,7 @@ const tetrisFactory = (function() {
     return {
       down: down,
     };
-  }
-
-  // Tetris.prototype.isWithinRange = function() {
-  //   this.setRectangularRange();
-  //   let xVertices = this.createSquares().map( (square)  => square.getCartesianVertices('x'));
-  //   let yVertices = this.createSquares().map( (square)  => square.getCartesianVertices('y'));
-  //   // console.log(isWithinRange(yVertices, this.range.up, this.range.down) )
-  //   if( isWithinRange(xVertices, this.range.left, this.range.right) &&
-  //     isWithinRange(yVertices, this.range.up, this.range.down) 
-  //     ) {
-  //       return true
-  //     }
-  //   return false;
-  // };
+  };
 
   // --------- TETRIS TRANSFORMATIONS ----------
 
@@ -458,18 +340,22 @@ const game = (function() {
     const startPoint = largeCanvas.config.startPoints[0];
     let currentInstance;
 
-    const placeOnStart = function() {
-
+    function correctStartAndPlace() {
       currentInstance = tetrisFactory.produce(nameOfFirst(), startPoint);
-      // if (currentInstance.getCartesianVertices())
-      // console.log( currentInstance.createSquares().map(square => square.getCartesianVertices() ))
-      return currentInstance;
+      let allVertices = currentInstance.createSquares().map(square => square.getCartesianVertices() )
+      if (!missesModuleOn(allVertices, 10)) {
+        return 
+      } else {
+        startPoint [missesModuleOn(allVertices, 10)] += 5;
+        console.log("Corrected on "+ missesModuleOn(allVertices, 10))
+      };
+      correctStartAndPlace();
     };
     const getInstance = function() {
       return currentInstance;
     };
     return {
-      placeOnStart: placeOnStart,
+      placeOnStart: correctStartAndPlace,
       getInstance: getInstance,
     };
   })();
