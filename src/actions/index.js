108aba0710dfc7f999af71659ce82cb9aaf00138
MaciejReimann <maciej.reimann@gmail.com>
2018-10-01 22:03:13 +0200

    key events dispatch actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,29 @@
 import { MOVE, START, PAUSE, INCREMENT } from "./types";
 
 export const moveTetris = direction => {
+  console.log(direction);
+  let nextState = {};
+  // dispatch(incrementScore());
   return {
     type: MOVE,
-    payload: direction
+    payload: nextState
   };
 };
 
 export const incrementScore = () => {
   return {
     type: INCREMENT
+  };
+};
+
+export const startGame = () => {
+  return {
+    type: START
+  };
+};
+
+export const pauseGame = () => {
+  return {
+    type: PAUSE
   };
 };
--- src/containers/LargeCanvas.js
+++ src/containers/LargeCanvas.js
@@ -2,11 +2,39 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 
-import { moveTetris } from "../actions/index";
+import { moveTetris, startGame, pauseGame } from "../actions/index";
 import Canvas from "../components/Canvas";
 
 class LargeCanvas extends Component {
-  componentDidMount() {}
+  componentDidMount() {
+    window.addEventListener("keydown", e => {
+      switch (e.key) {
+        case "ArrowLeft":
+          this.props.moveTetris("LEFT");
+          break;
+        case "ArrowDown":
+          this.props.moveTetris("DOWN");
+          break;
+        case "ArrowRight":
+          this.props.moveTetris("RIGHT");
+          break;
+        case "a":
+        case "A":
+          this.props.moveTetris("ROTATE_RIGHT");
+          break;
+        case "z":
+        case "Z":
+          this.props.moveTetris("ROTATE_LEFT");
+          break;
+        case "Enter":
+          this.props.startGame();
+          break;
+        case " ":
+          this.props.pauseGame();
+          break; // spacebar
+      }
+    });
+  }
   render() {
     return (
       <div className="large-canvas">
@@ -30,7 +58,10 @@ function mapDispatchToProps(dispatch) {
   // if moveTetris action creator is being called INSIDE THIS container
   // , the result,
   // i.e. the action, flows through all reducers
-  return bindActionCreators({ moveTetris: moveTetris }, dispatch);
+  return bindActionCreators(
+    { moveTetris: moveTetris, startGame: startGame, pauseGame: pauseGame },
+    dispatch
+  );
 }
 
 // propotes LargeCanvas to the container, that has to know about dispatch,
